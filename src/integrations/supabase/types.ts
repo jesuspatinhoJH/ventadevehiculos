8e89e991093b9a07c115b98c1adf8e45c9c68daf
jesuspatinhoJH <79804484+jesuspatinhoJH@users.noreply.github.com>
2026-04-29 14:00:30 -0600

    Add files via upload

---
+++ src/integrations/supabase/types.ts
@@ -0,0 +1,451 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "14.1"
+  }
+  public: {
+    Tables: {
+      allowed_domains: {
+        Row: {
+          created_at: string
+          domain: string
+          id: string
+        }
+        Insert: {
+          created_at?: string
+          domain: string
+          id?: string
+        }
+        Update: {
+          created_at?: string
+          domain?: string
+          id?: string
+        }
+        Relationships: []
+      }
+      brand_logos: {
+        Row: {
+          brand_name: string
+          created_at: string
+          created_by: string | null
+          id: string
+          logo_url: string
+        }
+        Insert: {
+          brand_name: string
+          created_at?: string
+          created_by?: string | null
+          id?: string
+          logo_url: string
+        }
+        Update: {
+          brand_name?: string
+          created_at?: string
+          created_by?: string | null
+          id?: string
+          logo_url?: string
+        }
+        Relationships: []
+      }
+      user_roles: {
+        Row: {
+          id: string
+          role: Database["public"]["Enums"]["app_role"]
+          user_id: string
+        }
+        Insert: {
+          id?: string
+          role: Database["public"]["Enums"]["app_role"]
+          user_id: string
+        }
+        Update: {
+          id?: string
+          role?: Database["public"]["Enums"]["app_role"]
+          user_id?: string
+        }
+        Relationships: []
+      }
+      vehicles: {
+        Row: {
+          brand: string
+          buyer_name: string | null
+          color: string
+          created_at: string
+          created_by: string | null
+          description: string
+          id: string
+          images: string[]
+          img: string
+          is_active: boolean
+          is_armored: boolean
+          is_public: boolean
+          location: string
+          mileage: string
+          name: string
+          plate_state: string
+          price_employee: number
+          price_public: number
+          release_at_public: string | null
+          sale_notes: string | null
+          slug: string
+          sold_at: string | null
+          sold_price: number | null
+          status: string
+          type: string
+          vin: string
+          year: number
+        }
+        Insert: {
+          brand: string
+          buyer_name?: string | null
+          color?: string
+          created_at?: string
+          created_by?: string | null
+          description?: string
+          id?: string
+          images?: string[]
+          img?: string
+          is_active?: boolean
+          is_armored?: boolean
+          is_public?: boolean
+          location?: string
+          mileage?: string
+          name: string
+          plate_state?: string
+          price_employee: number
+          price_public: number
+          release_at_public?: string | null
+          sale_notes?: string | null
+          slug: string
+          sold_at?: string | null
+          sold_price?: number | null
+          status?: string
+          type: string
+          vin?: string
+          year: number
+        }
+        Update: {
+          brand?: string
+          buyer_name?: string | null
+          color?: string
+          created_at?: string
+          created_by?: string | null
+          description?: string
+          id?: string
+          images?: string[]
+          img?: string
+          is_active?: boolean
+          is_armored?: boolean
+          is_public?: boolean
+          location?: string
+          mileage?: string
+          name?: string
+          plate_state?: string
+          price_employee?: number
+          price_public?: number
+          release_at_public?: string | null
+          sale_notes?: string | null
+          slug?: string
+          sold_at?: string | null
+          sold_price?: number | null
+          status?: string
+          type?: string
+          vin?: string
+          year?: number
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      vehicles_employee: {
+        Row: {
+          brand: string | null
+          color: string | null
+          created_at: string | null
+          created_by: string | null
+          description: string | null
+          id: string | null
+          images: string[] | null
+          img: string | null
+          is_active: boolean | null
+          is_armored: boolean | null
+          is_public: boolean | null
+          location: string | null
+          mileage: string | null
+          name: string | null
+          plate_state: string | null
+          price_employee: number | null
+          price_public: number | null
+          release_at_public: string | null
+          slug: string | null
+          status: string | null
+          type: string | null
+          year: number | null
+        }
+        Insert: {
+          brand?: string | null
+          color?: string | null
+          created_at?: string | null
+          created_by?: string | null
+          description?: string | null
+          id?: string | null
+          images?: string[] | null
+          img?: string | null
+          is_active?: boolean | null
+          is_armored?: boolean | null
+          is_public?: boolean | null
+          location?: string | null
+          mileage?: string | null
+          name?: string | null
+          plate_state?: string | null
+          price_employee?: number | null
+          price_public?: number | null
+          release_at_public?: string | null
+          slug?: string | null
+          status?: string | null
+          type?: string | null
+          year?: number | null
+        }
+        Update: {
+          brand?: string | null
+          color?: string | null
+          created_at?: string | null
+          created_by?: string | null
+          description?: string | null
+          id?: string | null
+          images?: string[] | null
+          img?: string | null
+          is_active?: boolean | null
+          is_armored?: boolean | null
+          is_public?: boolean | null
+          location?: string | null
+          mileage?: string | null
+          name?: string | null
+          plate_state?: string | null
+          price_employee?: number | null
+          price_public?: number | null
+          release_at_public?: string | null
+          slug?: string | null
+          status?: string | null
+          type?: string | null
+          year?: number | null
+        }
+        Relationships: []
+      }
+      vehicles_public: {
+        Row: {
+          brand: string | null
+          color: string | null
+          created_at: string | null
+          description: string | null
+          id: string | null
+          images: string[] | null
+          img: string | null
+          is_armored: boolean | null
+          is_public: boolean | null
+          location: string | null
+          mileage: string | null
+          name: string | null
+          plate_state: string | null
+          price_public: number | null
+          slug: string | null
+          status: string | null
+          type: string | null
+          year: number | null
+        }
+        Insert: {
+          brand?: string | null
+          color?: string | null
+          created_at?: string | null
+          description?: string | null
+          id?: string | null
+          images?: string[] | null
+          img?: string | null
+          is_armored?: boolean | null
+          is_public?: boolean | null
+          location?: string | null
+          mileage?: string | null
+          name?: string | null
+          plate_state?: string | null
+          price_public?: number | null
+          slug?: string | null
+          status?: string | null
+          type?: string | null
+          year?: number | null
+        }
+        Update: {
+          brand?: string | null
+          color?: string | null
+          created_at?: string | null
+          description?: string | null
+          id?: string | null
+          images?: string[] | null
+          img?: string | null
+          is_armored?: boolean | null
+          is_public?: boolean | null
+          location?: string | null
+          mileage?: string | null
+          name?: string | null
+          plate_state?: string | null
+          price_public?: number | null
+          slug?: string | null
+          status?: string | null
+          type?: string | null
+          year?: number | null
+        }
+        Relationships: []
+      }
+    }
+    Functions: {
+      get_vehicle_vin: { Args: { _slug: string }; Returns: string }
+      has_role: {
+        Args: {
+          _role: Database["public"]["Enums"]["app_role"]
+          _user_id: string
+        }
+        Returns: boolean
+      }
+      reserve_vehicle: { Args: { _slug: string }; Returns: undefined }
+    }
+    Enums: {
+      app_role: "admin" | "employee" | "user"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {
+      app_role: ["admin", "employee", "user"],
+    },
+  },
+} as const
